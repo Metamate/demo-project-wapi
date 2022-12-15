f421147ce4f327d189a7f9f483a35027ac27ca07
Metamate <jakikob@gmail.com>
2022-12-15 12:16:15 +0100

    .\.dockerignore

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace demo_project_wapi.Controllers;
 
 [ApiController]
@@ -25,7 +23,7 @@ public class WeatherForecastController : ControllerBase
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)] + "!!"
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)] + "!!!"
         })
         .ToArray();
     }
